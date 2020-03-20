5bbd5a5179523db930410b52647505c8cf43b6b6
Encripse <yunussutanti90@gmail.com>
2020-03-20 17:43:15 +0700

    Characther Movement
    making a characterController, there are a gravity manager script, for now it's unnesesary.

---
+++ OWO/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+    public float jumpV;
+
+    [HideInInspector]
+    public Rigidbody2D player;
+
+    private void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jumping();
+        }
+    }
+
+    void Movement()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        moveInput = (moveInput * speed) * Time.deltaTime;
+
+        //The Movement
+        player.velocity = new Vector2(moveInput, player.velocity.y);
+    }
+
+    void Jumping()
+    {
+        player.velocity = new Vector2(player.velocity.x, jumpV * Time.deltaTime);
+    }
+}
+
---
+++ OWO/Assets/Scripts/GravityManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityManager : MonoBehaviour
+{
+    public CharacterController player;
+    public float gravityMutliplier;
+
+    private void Update()
+    {
+        GravityController();
+    }
+
+    void GravityController()
+    {
+        if (player.player.velocity.y < 0f) 
+        {
+            player.player.velocity = new Vector2(player.player.velocity.x, (Physics2D.gravity.y * gravityMutliplier));
+        }else if (player.player.velocity.y > 0f)
+        {
+            player.player.velocity = new Vector2(player.player.velocity.x, (Physics2D.gravity.y * gravityMutliplier));
+        }
+    }
+}
